0d9c68eff8703a9fb8ea3700dcd6304f1dc8bb25
THANETNove <novel2020manga@gmail.com>
2024-08-12 20:35:43 +0700

    add

--- login.php
+++ login.php
@@ -1,48 +1,43 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "traverWeb";
-/* include 'db_config.php'; */
+include 'db_config.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die(json_encode(['error' => "Connection failed: " . $conn->connect_error]));
 }
 
-if (isset($_POST['isAdd']) && $_POST['isAdd'] == 'true') {
-    $input_username = $_POST['username'];
-    $input_password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // Fetch user with given username
-    $sql = "SELECT * FROM users WHERE username = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $input_username);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    if (isset($_POST['isAdd']) && $_POST['isAdd'] == 'true') {
+        $input_email = $_POST['email'];
+        $input_password = $_POST['password'];
+        // Fetch user with given username
+        $sql = "SELECT * FROM users WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $input_email);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    // If user exists
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
+        // If user exists
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
 
-        // Verify the password
-        if (password_verify($input_password, $user['password'])) {
-            unset($user['password']); // Removing password from the response for security reasons
-            echo json_encode(['message' => 'Login successful!', 'user' => $user]);
+            // Verify the password
+            if (password_verify($input_password, $user['password'])) {
+                unset($user['password']); // Removing password from the response for security reasons
+                echo json_encode(['message' => 'Login successful!', 'user' => $user]);
+            } else {
+                echo json_encode(['error' => 'Incorrect password!']);
+            }
         } else {
-            echo json_encode(['error' => 'Incorrect password!']);
+            echo json_encode(['error' => 'Login not found!']);
         }
     } else {
-        echo json_encode(['error' => 'Login not found!']);
+        echo json_encode(['error' => 'Invalid request!']);
     }
-} else {
-    echo json_encode(['error' => 'Invalid request!']);
 }
 
-$conn->close();
+//$conn->close();
